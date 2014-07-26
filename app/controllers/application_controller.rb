e15c68f16bd9a93390649a2439876a5299505776
kadotami <mondamin.kqfb@gmail.com>
2014-07-26 14:12:01 +0900

    this is release version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,19 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   include ApplicationHelper
 
-  #例外処理
-  # rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  # rescue_from ActionController::RoutingError, with: :render_404
+#例外処理
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
   # rescue_from Exception, with: :render_500
 
 
-  # def render_404(exception = nil)
-  #   if exception
-  #     logger.info "Rendering 404 with exception: #{exception.message}"
-  #   end
+  def render_404(exception = nil)
+    if exception
+      logger.info "Rendering 404 with exception: #{exception.message}"
+    end
 
-  #   render :template => "errors/404", :status => 404, :layout => 'application', :content_type => 'text/html'
-  # end
+    render :template => "errors/404", :status => 404, :layout => 'application', :content_type => 'text/html'
+  end
 
   # def render_500(exception = nil)
   #   if exception
