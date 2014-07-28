3d0b0bf4f400113b9dfbe92a06f424969e6d703f
kadotami <mondamin.kqfb@gmail.com>
2014-07-28 16:04:01 +0900

    edit design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 #例外処理
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
-  # rescue_from Exception, with: :render_500
+  rescue_from Exception, with: :render_500
 
 
   def render_404(exception = nil)
@@ -19,11 +19,11 @@ class ApplicationController < ActionController::Base
     render :template => "errors/404", :status => 404, :layout => 'application', :content_type => 'text/html'
   end
 
-  # def render_500(exception = nil)
-  #   if exception
-  #     logger.info "Rendering 500 with exception: #{exception.message}"
-  #   end
+  def render_500(exception = nil)
+    if exception
+      logger.info "Rendering 500 with exception: #{exception.message}"
+    end
 
-  #   render :template => "errors/500", :status => 500, :layout => 'application'
-  # end
+    render :template => "errors/500", :status => 500, :layout => 'application'
+  end
 end
